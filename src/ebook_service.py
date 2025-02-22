fa36fe248ae89dc90a62b3484866f5b302709eac
Yeonuk Hwang <charlie.hwang131@gmail.com>
2025-02-23 03:34:46 +0900

    fix: modify ebook_parser to be more robust
    - set scale to page-fit before taking a screenshot
    - modify the book_current_page_locator correctly, data-page-label starts
    from the actual contents, which results in skipping covers and index.
    Therefore, modify the selector to be based on data-page-number which
    starts from the cover.
    - only trigger navigate to the next page when current page is not last
    page, triggering this function in last page will lead to raise an error
    since the button is not clickable in the last page.

--- src/ebook_service.py
+++ src/ebook_service.py
@@ -13,22 +13,24 @@ class EbookService:
     async def capture_all_pages(self) -> list[str]:
         screenshot_paths: list[str] = []
 
+        await self._parser.set_page_fit_scale()
+
         for index in range(await self._parser.get_total_pages()):
             current_page = index + 1
 
             file_path = f"images/{current_page}.png"
 
             book_current_page_locator = self._page.locator(
-                f'[data-page-label="{current_page}"]'
+                f'.page[data-page-number="{current_page}"]'
             )
 
             _ = await book_current_page_locator.screenshot(path=file_path)
 
-            await self._parser.navigate_to_next_page()
-
             screenshot_paths.append(file_path)
+
             print(f"Screenshot saved as {file_path}")
 
-            await asyncio.sleep(1)
+            if not await self._parser.is_last_page():
+                await self._parser.navigate_to_next_page()
 
         return screenshot_paths
